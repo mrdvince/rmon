892100f3714963e85a8259326f301267cd62493f
Vince <vincivenv@gmail.com>
2023-08-13 19:12:21 +0200

    Identifiers, Longer test

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -29,6 +29,20 @@ impl Lexer {
         self.position = self.read_postion;
         self.read_postion += 1;
     }
+
+    fn read_identifier(&mut self) -> String {
+        let start_position = self.position;
+        while let Some(ch) = self.ch {
+            if is_letter(ch) {
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+        self.input[start_position..self.position].to_string()
+    }
+
+
     pub fn next_token(&mut self) -> Token {
         let tok = match self.ch {
             Some('=') => new_token(Tokens::ASSIGN.as_str().to_string(), self.ch.unwrap()),
@@ -43,9 +57,16 @@ impl Lexer {
                 r#type: Tokens::EOF.as_str().to_string(),
                 literal: "".to_string(),
             },
+            Some(ch) if is_letter(ch) => {
+                let literal = self.read_identifier();
+                Token {
+                    r#type: Tokens::IDENT.as_str().to_string(), // Assuming IDENT represents identifiers
+                    literal,
+                }
+            }
             _ => Token {
                 r#type: Tokens::ILLEGAL.as_str().to_string(),
-                literal: self.ch.unwrap().to_string(),
+                literal: self.ch.map(|ch| ch.to_string()).unwrap_or_default(),
             },
         };
         self.read_char();
@@ -58,4 +79,8 @@ fn new_token(token_type: TokenType, ch: char) -> Token {
         r#type: token_type,
         literal: ch.to_string(),
     }
+}
+
+fn is_letter(ch: char) -> bool {
+    ch.is_ascii_alphabetic() || ch == '_'
 }
--- tests/lexer_tests.rs
+++ tests/lexer_tests.rs
@@ -3,18 +3,54 @@ use rmon::token::Tokens;
 
 #[test]
 fn test_next_token() {
-    let input = "=+(){},;";
+    let input = "let five = 5;\
+            \nlet ten = 10;\
+            \nlet add = fn(x, y) {\
+            \n  x + y;\
+            \n};\
+            \nlet result = add(five, ten);\
+            \n";
+
     let tests = [
+        (Tokens::LET.as_str(), "let"),
+        (Tokens::IDENT.as_str(), "five"),
         (Tokens::ASSIGN.as_str(), "="),
-        (Tokens::PLUS.as_str(), "+"),
+        (Tokens::INT.as_str(), "5"),
+        (Tokens::SEMICOLON.as_str(), ";"),
+        (Tokens::LET.as_str(), "let"),
+        (Tokens::IDENT.as_str(), "ten"),
+        (Tokens::ASSIGN.as_str(), "="),
+        (Tokens::INT.as_str(), "10"),
+        (Tokens::SEMICOLON.as_str(), ";"),
+        (Tokens::LET.as_str(), "let"),
+        (Tokens::IDENT.as_str(), "add"),
+        (Tokens::ASSIGN.as_str(), "="),
+        (Tokens::FUNCTION.as_str(), "fn"),
         (Tokens::LPAREN.as_str(), "("),
+        (Tokens::IDENT.as_str(), "x"),
+        (Tokens::COMMA.as_str(), ","),
+        (Tokens::IDENT.as_str(), "y"),
         (Tokens::RPAREN.as_str(), ")"),
         (Tokens::LBRACE.as_str(), "{"),
+        (Tokens::IDENT.as_str(), "x"),
+        (Tokens::PLUS.as_str(), "+"),
+        (Tokens::IDENT.as_str(), "y"),
+        (Tokens::SEMICOLON.as_str(), ";"),
         (Tokens::RBRACE.as_str(), "}"),
+        (Tokens::SEMICOLON.as_str(), ";"),
+        (Tokens::LET.as_str(), "let"),
+        (Tokens::IDENT.as_str(), "result"),
+        (Tokens::ASSIGN.as_str(), "="),
+        (Tokens::IDENT.as_str(), "add"),
+        (Tokens::LPAREN.as_str(), "("),
+        (Tokens::IDENT.as_str(), "five"),
         (Tokens::COMMA.as_str(), ","),
+        (Tokens::IDENT.as_str(), "ten"),
+        (Tokens::RPAREN.as_str(), ")"),
         (Tokens::SEMICOLON.as_str(), ";"),
         (Tokens::EOF.as_str(), ""),
     ];
+
     let mut l = Lexer::new(input);
     for (i, (expected_type, expected_literal)) in tests.iter().enumerate() {
         let tok = l.next_token();
