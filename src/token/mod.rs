21f56c05672a11a60c58b46694c68be770a428e8
Vince <vincivenv@gmail.com>
2023-08-13 17:48:22 +0200

    chore: Format, remove commented tokens

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -5,25 +5,6 @@ pub struct Token {
     pub literal: String,
 }
 
-// pub const ILLEGAL: &str = "ILLEGAL";
-// pub const EOF: &str = "EOF";
-// // identifiers and literals
-// pub const IDENT: &str = "IDENT";
-// pub const INT: &str = "INT";
-// // operators
-// pub const ASSIGN: &str = "ASSIGN";
-// pub const PLUS: &str = "PLUS";
-
-// // delimiters
-// pub const COMMA: &str = ",";
-// pub const SEMICOLON: &str = ";";
-// pub const LPAREN: &str = "(";
-// pub const RPAREN: &str = ")";
-// pub const LBRACE: &str = "{";
-// pub const RBRACE: &str = "}";
-// // keywords
-// pub const FUNCTION: &str = "FUNCTION";
-// pub const LET: &str = "LET";
 pub enum Tokens {
     ILLEGAL,
     EOF,
